b19774e4f82de3166630dbcb804daad455c12870
sunwer54 <1435793007@qq.com>
2022-05-05 00:11:42 +0800

    oasystem shiro身份验证，密码加密

--- manage/src/test/java/Demo.java
+++ manage/src/test/java/Demo.java
@@ -1,10 +1,5 @@
 import org.apache.shiro.crypto.hash.Md5Hash;
 
-/**
- * @author: 享学课堂-SaiLing老师
- * @老师qq: 2408524688
- * @description:
- */
 public class Demo {
     public static void main(String[] args) {
         /*
